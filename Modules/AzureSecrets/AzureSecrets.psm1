0bd504a74a930ddc5770d1344bc5ec806048f58e
nik.chikersal@automateyourpowershell.com <nik.chikersal@automateyourpowershell.com>
2023-12-27 23:03:23 -0500

    add modules

---
+++ Modules/AzureSecrets/AzureSecrets.psm1
@@ -0,0 +1,5 @@
+.$PSScriptRoot\Get-GraphAccessToken.ps1
+.$PSScriptRoot\Send-GraphEmail.ps1
+.$PSScriptRoot\Renew-ClientSecrets.ps1
+.$PSScriptRoot\Get-KeyVaultSecret.ps1
+.$PSScriptRoot\Invoke-GraphAPIRequest.ps1
