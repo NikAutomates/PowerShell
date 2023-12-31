1303f9937a1be5bf367955d5f33d4eedc2c027e2
nik.chikersal@automateyourpowershell.com <nik.chikersal@automateyourpowershell.com>
2023-12-31 16:32:11 -0500

    comit

--- Modules/AzureSecrets/AzureSecrets.psm1
+++
@@ -1,5 +0,0 @@
-.$PSScriptRoot\Get-GraphAccessToken.ps1
-.$PSScriptRoot\Send-GraphEmail.ps1
-.$PSScriptRoot\Renew-ClientSecrets.ps1
-.$PSScriptRoot\Get-KeyVaultSecret.ps1
-.$PSScriptRoot\Invoke-GraphAPIRequest.ps1
